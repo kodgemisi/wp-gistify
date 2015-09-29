3c59cced65c43e8d193338dff24d82931ee9cd9a
Destan Sarpkaya <destan@dorukdestan.com>
2015-09-29 14:55:10 +0300

    update gulp config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,18 +16,21 @@ gulp.task('styles', () => {
     }).on('error', $.sass.logError))
     .pipe($.sourcemaps.write())
     .pipe($.minifyCss({compatibility: '*'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('js', () => {
   return gulp.src(['src/js/gistify-button.js', 'src/js/gistify-render.js'])
     .pipe($.uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task('clean', del.bind(null, ['dist']));
+gulp.task('clean', del.bind(null, ['gistify-button.js', 'gistify-render.js', 'gistify-admin.css', 'gistify.php']));
 
 gulp.task('build', ['styles', 'js'], () => {
+  gulp.src('src/gistify.php')
+    .pipe(gulp.dest('.'));
+
   var ret = gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
   return ret;
 });
@@ -36,6 +39,8 @@ gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
 
+// Prepares a zippeble folder ready-to deploy to Wordpress
+// by pruning all unnecessary files
 gulp.task('dist', ['default'], () => {
-  del 'src';
+  del('src', 'node_modules', 'gulpfile.babel.js', 'package.json');
 });
